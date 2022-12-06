cf216216c9de014a43ab38bd2cfaffe337ac1dfb
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-06 19:42:51 +0200

    day4 Part Two

--- day4/solution.js
+++ day4/solution.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 const lines = fs.readFileSync('data.txt', {encoding: 'utf-8'}).split('\r\n');
 
-let counter = 0;
+// Part One
+let fullyOverlapping = 0;
 lines.map(e=>{
     let twoSetsOfNums = e.split(',');
     let firstSet = twoSetsOfNums.shift().split('-');
@@ -12,8 +13,27 @@ lines.map(e=>{
     let secondSetFirstNum = Number(secondSet.shift());
     let secondSetSecondNum = Number(secondSet.pop());
     if(firstSetFirstNum <= secondSetFirstNum && firstSetSecondNum >= secondSetSecondNum || firstSetFirstNum >= secondSetFirstNum && firstSetSecondNum <= secondSetSecondNum){
-        counter++
+        fullyOverlapping++
     }
 })
+console.log(fullyOverlapping)
 
-console.log(counter)
+// Part Two
+let total = lines.length;
+let nonOverlapping = 0;
+// find non-overlapping
+lines.map(e=>{
+    let twoSetsOfNums = e.split(',');
+    let firstSet = twoSetsOfNums.shift().split('-');
+    let firstSetFirstNum = Number(firstSet.shift());
+    let firstSetSecondNum = Number(firstSet.pop());
+    let secondSet = twoSetsOfNums.pop().split('-');
+    let secondSetFirstNum = Number(secondSet.shift());
+    let secondSetSecondNum = Number(secondSet.pop());
+    if(firstSetSecondNum < secondSetFirstNum || firstSetFirstNum > secondSetSecondNum){
+        nonOverlapping++
+    }
+})
+// get overalpping by substracting non-overlapping from total
+let overlapping = total - nonOverlapping
+console.log(overlapping)
