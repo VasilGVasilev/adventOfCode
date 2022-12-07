7fa2b0a482ea8e078d6a39bae665b8978ce3ba00
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-07 15:38:10 +0200

    day 5 part 2

--- day5/solution.js
+++ day5/solution.js
@@ -38,3 +38,41 @@ function eachRestack(stacks, fromStack, toStack, itemsMoved){
     stacks[toStack] = newStack;
     
 }
+
+
+// PART Two
+
+inputs = inputs.map((e)=>{
+    let line = e.split(' ');
+    line.shift();
+    let itemsMoved = Number(line.shift());
+    line.shift();
+    let fromStack = line.shift();
+    line.shift();
+    let toStack = line.shift();
+    eachRestack(stacks, fromStack, toStack, itemsMoved);
+    
+})
+
+let resultStr = '';
+for (let key in stacks){
+    let letter = stacks[key].slice(-1);
+    resultStr += letter
+}
+
+console.log(resultStr);
+
+function eachRestack(stacks, fromStack, toStack, itemsMoved){
+    let manipulatedStack = stacks[fromStack].split('');
+    
+    let currentItems = [];
+    for (let i = 0; i < itemsMoved; i++){
+        currentItems.unshift(manipulatedStack.pop());
+    }
+    stacks[fromStack] = stacks[fromStack].slice(0, -itemsMoved)
+    currentItems = currentItems.join('');
+    let newStack = stacks[toStack];
+    newStack += currentItems;
+    stacks[toStack] = newStack;
+    
+}
