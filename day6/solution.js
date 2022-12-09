68f6d8955e01a9121a66a138913c749694f3f84e
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-09 09:45:07 +0200

    day6 part one

---
+++ day6/solution.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+let input = fs.readFileSync('data.txt', {encoding: 'utf-8'})
+
+// Part One
+
+counter = 4;
+for (let i = 0; i < input.length; i++){
+    
+    let segmentOfFour = input.substring(i,i+5);
+    if(hasRepeats(segmentOfFour)){
+        counter++;
+        continue;
+    } else {
+        break;
+    }
+}
+
+console.log(counter);
+
+function hasRepeats(str) {
+    return /(.).*\1/.test(str);
+}
