3636275522bde2ed01d061f6f21a687ee61f316f
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-09 10:11:38 +0200

    day6 part two

--- day6/solution.js
+++ day6/solution.js
@@ -1,12 +1,32 @@
 const fs = require('fs');
 let input = fs.readFileSync('data.txt', {encoding: 'utf-8'})
 
-// Part One
+// // Part One
 
 counter = 4;
 for (let i = 0; i < input.length; i++){
     
     let segmentOfFour = input.substring(i,i+5);
+    if(hasRepeats(segmentOfFour)){
+        counter++;
+        continue;
+    } else {
+        break;
+    }
+}
+
+console.log(counter);
+
+function hasRepeats(str) {
+    return /(.).*\1/.test(str);
+}
+
+// Part Two
+
+counter = 14;
+for (let i = 0; i < input.length; i++){
+    
+    let segmentOfFour = input.slice(i, i + 14);
     if(hasRepeats(segmentOfFour)){
         counter++;
         continue;
